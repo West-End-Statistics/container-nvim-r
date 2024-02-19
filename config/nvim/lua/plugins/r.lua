3c14ab7c0a8200f072525975bd82776e5ed2175a
Druker, Vitaly <vitaly.druker@astrazeneca.com>
2024-02-19 11:26:42 -0500

    got omni completion working

--- config/nvim/lua/plugins/r.lua
+++ config/nvim/lua/plugins/r.lua
@@ -12,5 +12,64 @@ return {
 	]])
 end
 },
+  {
+    	"hrsh7th/nvim-cmp",
+      dependencies = {
+        "jalvesaq/cmp-nvim-r",
+        "hrsh7th/nvim-cmp",
+        "onsails/lspkind.nvim"
+      },
+      lazy = false,
+      config = function()
+        local cmp = require("cmp")
+        local lspkind = require('lspkind')
+
+
+        cmp.setup {
+    mapping = cmp.mapping.preset.insert({
+          ['<Tab>'] = {
+            i = cmp.mapping.confirm({ select = true }),
+    },
+    }),
+
+
+              formatting = {
+        fields = {'abbr', 'kind', 'menu'},
+        format = lspkind.cmp_format({
+            mode = 'symbol', -- show only symbol annotations
+            maxwidth = 50, -- prevent the popup from showing more than provided characters
+            ellipsis_char = '...', -- the truncated part when popup menu exceed maxwidth
+            before = function(entry, item)
+                local menu_icon = {
+                    nvim_lsp = '',
+                    vsnip = '',
+                    path = '',
+                    --cmp_zotcite = 'z',
+                    cmp_nvim_r = 'R'
+                }
+                item.menu = menu_icon[entry.source.name]
+                return item
+            end,
+        })
+
+          
+      },
+      sources = {
+    { name = 'cmp_nvim_r' },
+    { name = 'path', option = { trailing_slash = true } },
+    { name = 'nvim_lsp' },
+  }
+}
+        require("cmp_nvim_r").setup({
+  filetypes = {"r", "rmd", "quarto", "rhelp"},
+  doc_width = 58
+  })
+
+
+      end
+
+  }
+
+
 
 }
--- test.R
+++ test.R
@@ -2,11 +2,25 @@ library(foreign)
 
 
 
+
+
+model.extract
 test_function <- function(x = 1, y = 2) {
 
 	x + y
 }
 
 
+
+x <- data.frame("dumb name" = 123)
+
+colnames(x) <- "dumb name"
+
+
 httpgd::hgd(host = "0.0.0.0", port = 6543)
 httpgd::hgd_url(host = "localhost")
+xx <- data.frame(
+           x = LETTERS
+           )
+
+
